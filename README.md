ccb9c6e31e03463b2c368cb0f6dacb55b963a555
Peet Janse van Vuuren <peetjvv93@gmail.com>
2023-08-26 16:07:28 +0200

    repo setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Geo Data Map
+
+[![Checks](https://github.com/peetjvv/geo-data-map/actions/workflows/checks.yaml/badge.svg)](https://github.com/peetjvv/geo-data-map/actions/workflows/checks.yaml)
+
+Pet project to play around with Mapbox with the idea of eventually centralising and visualising various open source geographic datasets.
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import './scss/main.scss';
+
+const App: React.FC = () => {
+  return <div className="content">Howdy!</div>;
+};
+
+const container = document.getElementById('app');
+const root = createRoot(container!); // eslint-disable-line @typescript-eslint/no-non-null-assertion
+root.render(<App />);
